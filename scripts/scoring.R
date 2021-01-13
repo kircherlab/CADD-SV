11bc8b8c6377d18b2a34c2a12e99d163875189d4
kleinertp <38033071+kleinertp@users.noreply.github.com>
2021-01-13 16:30:31 +0100

    Update scoring.R

--- scripts/scoring.R
+++ scripts/scoring.R
@@ -3,23 +3,65 @@
 args = commandArgs(trailingOnly=TRUE)
 
 sink(file=NULL)
-load("model.RData")
-x=as.matrix(read.table(args[1],header=T))
 
-for(i in 1:108){
-  x[,i]=as.numeric(x[,i])
+
+cadd.sv.read=function(x,z){
+  
+y=as.matrix(read.table(paste(x,"/matrix",z,".bed",sep=""),header=T))
+
+header=colnames(x)
+
+y[,1]=gsub('[chr\n]', '', y[,1])
+
+for(i in 1:130){
+  y[,i]=as.numeric(y[,i])
 }
 
-x[is.na(x)] <- 0
-x=apply(x,2, as.numeric)
-x[is.na(x)] <- 0
+y[is.na(y)] <- 0
+y=apply(y,2, as.numeric)
+y[is.na(y)] <- 0
+
+ylen=y[,3]-y[,2]
+y[which(y[,43]>3000),44]=3000 #DNAse outliers
+y[which(y[,126]>1),126]=1 #DDD outliers
+
+for(i in c(73,98,102,106,110,114,118,121,122,123,5,7,9,11,42,43,45,47,49,51,53,55,57,59,61,63,65,67,69,131,85,86,87)){ #logs of distances and sums and counts
+  y[,i]=round(log10(abs(y[,i])+1),4)
+}
+
+y=y[,-c(73,98,102,106,110,114,118)]
+
+return(y)
+}
 
 
-data=as.data.frame(x[,4:108])
 
-pred=predict(model, data,type="response")
+caddsv=function(x){
+  k=list()
+  y=list()
+  k[[1]]=cadd.sv.read(x,z="")
+  k[[2]]=cadd.sv.read(x,z="_100kbup")
+  k[[3]]=cadd.sv.read(x,z="_100kbdown")
+  k[[4]]=k[[2]]+k[[3]]
+  k[[4]][,114]=apply(cbind(k[[2]][,114],k[[3]][,114]),1,min)
+  k[[4]][,115]=apply(cbind(k[[2]][,115],k[[3]][,115]),1,min)
+  k[[4]][,116]=apply(cbind(k[[2]][,116],k[[3]][,116]),1,min)
+  y[[1]]=k[[1]]
+  y[[2]]=k[[4]]
+  return(y)
+  }
 
-write.table(cbind(x[,1:3],pred),args[2],sep="\t",
+
+load("model.RData")
+set1=caddsv(args[1])
+
+f=dim(set1)[2] #features -3
+
+predict1.cdel=predict(cdel.model,set1[[1]][,4:f])
+predict2.hdel=predict(hdel.model,set1[[2]][,4:f])
+pred=apply(cbind(predict1.cdel,predict2.hdel),1,min)
+
+write.table(cbind(set1[,1:3],pred),args[2],sep="\t",
             row.names = F, 
             col.names = F, 
             quote = F)
