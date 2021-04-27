efabac09b885d8c13933fd6a73784a8b262d28d3
kleinertp <38033071+kleinertp@users.noreply.github.com>
2021-04-27 21:31:57 +0200

    Update scoring.R

--- scripts/scoring.R
+++ scripts/scoring.R
@@ -54,8 +54,8 @@ caddsv=function(x){
   k[[1]]=cadd.sv.read(x,z="")
   k[[2]]=cadd.sv.read(x,z="_100bpup")
   k[[3]]=cadd.sv.read(x,z="_100bpdown")
-  z1=match(k[[3]][,3]-100,k[[1]][,3])
-  z2=match(k[[3]][,3]-101,k[[1]][,3])
+  z1=match(k[[1]][,3],k[[3]][,3]-100)
+  z2=match(k[[1]][,3],k[[3]][,3]-101)
   z=cbind(z1,z2)
   z[is.na(z)] <- 0
   t=apply(z,1,max)
